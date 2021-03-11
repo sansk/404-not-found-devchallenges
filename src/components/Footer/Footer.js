f0ced158045d9078072fd53a2ef81dedaf298265
Sangy K <sansk@users.noreply.github.com>
2021-03-11 19:29:28 +0530

    Updated Footer links

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Footer() {
   return (
-    <p className='p-4 font-sans font-normal text-base text-gray-400 text-center'>created by <span className="font-bold">@sansk</span> - devChallenges.io</p>
+    <p className='p-4 font-sans font-normal text-base text-gray-400 text-center'>created by <a href="https://github.com/sansk" className="font-bold">Sangy K</a> - <a href="https://devchallenges.io">Dev Challenges</a></p>
   );
 }
 
